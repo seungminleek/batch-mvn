f0e08739c888c7daeb3ea2d0f5f9e5d6fd9d904e
seungminleek <fnfndia96@naver.com>
2021-09-26 17:52:59 +0900

    xml 리턴 형식 수정중

--- src/main/java/com/example/batchmvn/BatchJob.java
+++ src/main/java/com/example/batchmvn/BatchJob.java
@@ -57,16 +57,18 @@ public class BatchJob {
 
     /**
      * 하나은행 rest api 호출 부분
-     * @return
+     * @return Step
      */
     @Bean
     public Step step1() {       //TODO: POST 방식으로 바꿔야함, json 형태로 request 하는 부분도 필요
         ResponseEntity<ExchangeDto[]> response = restTemplate.getForEntity(bankApiUrl, ExchangeDto[].class );
-        Map rqData = new HashMap<>();
+        // Map rqData = new HashMap<String, Object>();
 
         // 하나은행에서 받아온 json 데이터를 엔티티로 변환
         ExchangeDto[] resultData = response.getBody();  // Body값을 받아서 buffer에 저장
-        exchangeData = Arrays.asList(resultData);       // LIST 형태로 변환
+        if (resultData != null) {
+            exchangeData = Arrays.asList(resultData);       // LIST 형태로 변환
+        }
 
         if (exchangeData.size() > 0) {
             testData = exchangeData.get(0).getName();
@@ -82,7 +84,7 @@ public class BatchJob {
     /**
      * XML 파일 생성하기
      * 일단 여기 메소드로 만들어서 단독 로직파일로 빼내기
-     * @return
+     * @return Step
      */
     @Bean
     public Step step2() {
@@ -92,21 +94,26 @@ public class BatchJob {
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
             Document doc = docBuilder.newDocument();
-            doc.setXmlStandalone(true); //standalone="no" 를 없애준다.
+            doc.setXmlStandalone(true);
 
             // 요소 생성시작
-            Element exInformation = doc.createElement("exInformation");
-            doc.appendChild(exInformation);
-            Element effectiveDate = doc.createElement("effectiveDate");
-            String dateTime = dtoData.getDate() + " " + dtoData.getTime();
-            effectiveDate.appendChild(doc.createTextNode(dateTime));
-            exInformation.appendChild(effectiveDate);     // 1.고시날짜
-            Element currencyCode = doc.createElement("currencyCode");
-            currencyCode.appendChild(doc.createTextNode(dtoData.getCurrencyCode()));
-            exInformation.appendChild(currencyCode);    // 2.통화코드
-            Element exchangeRate = doc.createElement("exchangeRate");
-            exchangeRate.appendChild(doc.createTextNode(dtoData.getTtBuyingPrice()));
-            exInformation.appendChild(exchangeRate);    // 2.통화코드
+            Element exchangeRates = doc.createElement("ExchangeRates");
+            doc.appendChild(exchangeRates);
+            Element exchangeRate = doc.createElement("ExchangeRate");
+            exchangeRates.appendChild(exchangeRate);
+            Element from = doc.createElement("From");
+            from.appendChild(doc.createTextNode(dtoData.getCurrencyCode()));
+            exchangeRate.appendChild(from);    // 1. 통화코드
+            Element to = doc.createElement("To");
+            to.appendChild(doc.createTextNode("KRW"));
+            exchangeRate.appendChild(to);
+            Element effectiveDate = doc.createElement("Effective_Date");
+            String dateStr = dtoData.getDate() + " " + dtoData.getTime();
+            effectiveDate.appendChild(doc.createTextNode(dateStr));
+            exchangeRate.appendChild(effectiveDate);
+//            Element exchangeRate = doc.createElement("exchangeRate");
+//            exchangeRate.appendChild(doc.createTextNode(dtoData.getTtBuyingPrice()));
+//            exchangeRates.appendChild(exchangeRate);    // 2.통화코드
 
             // XML 파일로 쓰기
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
@@ -121,11 +128,22 @@ public class BatchJob {
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
             String fileNm = simpleDateFormat.format(new Date());
             StreamResult result = new StreamResult(    // 파일경로지정
-                    new FileOutputStream(new File("/workspace/batch-mvn/src/main/resources/response/exchangeData.xml")));
+                    new FileOutputStream(new File("D://Asianaiidt/batch-mvn/src/main/resources/response/exchangeData.xml")));
 
             transformer.transform(source, result);
 
             log.info((BATCH_NAME + "========= xml 파일 생성 완료 ========="));
+
+            /* 20210919 todo
+             * 1. ibs xml 파일 가지고오기 -> 파일구조
+             * 2. sftp 서버 접속 및 업로드 하는 방법
+             * 3. log 생성하는 방법
+             *  1) 하나은행 api 호출 로그
+             *  2) 업로드된 로그
+             * 4.
+             * 5. vdi 에서 구름 build run
+             * + ibs api document
+             */
 
         } catch (Exception e){
             e.printStackTrace();
